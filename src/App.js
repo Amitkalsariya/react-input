d1f6eb2ebdfe9b7162793126a4f34ff12c81d645
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-06-04 08:51:47 +0530

    D

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [text, setText]=useState([])
+  const [inputvalue, setInputvalue] = useState()
+  const [text, setText] = useState([])
 
-  let dataAdd=()=>{
-    let copyData=[...text]  
-    copyData.push(inputValue)
-    setText(copyData)
+  let copyData = () => {
+    var data = [...text]
+    data.push(inputvalue)
+    setText(data)
+    setInputvalue("")
+
   }
+  const handleDelete = (i) => {
+    const newText = [...text];
+    newText.splice(i, 1);
+    setText(newText);
+    setInputvalue("")
+  };
+
+  const handleUpdate = (i, el) => {
+    const newText = [...text];
+    newText[i] = el;
+    setText(newText);
+    setInputvalue("")
+  };
 
   return (
     <div>
       <center>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(p)=>setInputValue(p.target.value)}
-        placeholder="Enter Something..."
-      />
-      <button onClick={dataAdd}>Click Me</button>
+      <input type="text" placeholder='Enter Value ..' value={inputvalue} onChange={(p) => { setInputvalue(p.target.value) }} />
+
+      <button onClick={copyData}>Click Me</button>
       {
-        text.map((el,i) => 
-          {
-            return(
-              <h1 key={i}>{el}</h1>
-            )
-          })
+        text.map((el, i) => {
+          return (
+            <div key={i}>
+            <h1>{el}</h1>
+            <button onClick={() => handleDelete(i)}>Delete</button>
+          <button onClick={() => handleUpdate(i, inputvalue)}>Update</button>
+            </div>
+          )
+        })
       }
-      </center>
+     
+     </center>
     </div>
   );
 }
