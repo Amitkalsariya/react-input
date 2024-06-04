afc3e7c2305f296458d7a76b22bd3d2f7f7751f2
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-06-04 09:44:32 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,46 @@
 import React, { useState } from 'react';
+import './App.css'; // Import your CSS file
 
 function App() {
-  const [inputvalue, setInputvalue] = useState()
+  const [inputvalue, setInputvalue] = useState('')
   const [text, setText] = useState([])
 
   let copyData = () => {
     var data = [...text]
     data.push(inputvalue)
     setText(data)
-    setInputvalue("")
-
+    setInputvalue('')
   }
+
   const dataDelete = (i) => {
     const data = [...text];
     data.splice(i, 1);
     setText(data);
-    setInputvalue("")
+    setInputvalue('')
   };
 
   const dataUpdate = (i, el) => {
     const data = [...text];
     data[i] = el;
     setText(data);
-    setInputvalue("")
+    setInputvalue('')
   };
 
   return (
-    <div>
-      <center>
-      <input type="text" placeholder='Enter Value ..' value={inputvalue} onChange={(p) => { setInputvalue(p.target.value) }} />
-
+    <div className="container"> {/* Apply class for centering */}
+      <input type="text" className="input-field" placeholder='Enter Value..' value={inputvalue} onChange={(p) => { setInputvalue(p.target.value) }} />
       <button onClick={copyData}>Click Me</button>
       {
         text.map((el, i) => {
           return (
-            <div key={i}>
-            <h1>{el}</h1>
-            <button onClick={() => dataDelete(i)}>Delete</button>
-          <button onClick={() => dataUpdate(i, inputvalue)}>Update</button>
+            <div key={i} className="item">
+              <h1>{el}</h1>
+              <button onClick={() => dataDelete(i)}>Delete</button>
+              <button onClick={() => dataUpdate(i, inputvalue)}>Update</button>
             </div>
           )
         })
       }
-     
-     </center>
     </div>
   );
 }
